c60beb33b098228f43b3e138e3ab7b18f40eddb8
Sarah P <poultonse@gmail.com>
2021-08-24 16:01:44 -0500

    initial commit

---
+++ src/actions/authUser.js
@@ -0,0 +1,7 @@
+export const SET_AUTHUSER= "SET_AUTHUSER";
+
+export function setAuthUser (id){
+    return {
+        type: SET_AUTHUSER, id
+    }
+}
---
+++ src/actions/shared.js
@@ -0,0 +1,30 @@
+// import { receiveQuestions} from "./questions";
+// import { receiveUsers } from './users';
+import { setAuthUser } from "./authUser";
+import { showLoading, hideLoading } from "react-redux-loading";
+import {_getQuestions, _getUsers} from "../utils/_DATA";
+
+export const RECEIVE_DATA = "RECEIVE_DATA";
+
+export function receiveData(users, questions){
+    return{
+        type: RECEIVE_DATA,
+        users,
+        questions,
+    }
+}
+
+const AUTH_USERID = 'sarahedo';
+
+export function handleInitialData(){
+    return(dispatch)=> {
+        dispatch(showLoading())
+        return Promise.all([
+            _getUsers(), _getQuestions()
+        ]).then(([users, questions])=> {
+            dispatch(receiveData(users, questions))
+            dispatch(setAuthUser(AUTH_USERID))
+            dispatch(hideLoading())
+        })
+    }
+}
