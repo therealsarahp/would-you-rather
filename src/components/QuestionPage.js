9b6ba4d9aa2d9641ba1b7fbdd87489d94b284430
Sarah P <poultonse@gmail.com>
2021-08-26 11:09:44 -0500

    progress bars added to after vote.. still ugly though

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import Question from "./Question";
 import {handleAnswerQuestion} from "../actions/shared";
 
+
 class QuestionPage extends Component{
 
     handleClick=(e)=>{
@@ -22,43 +23,71 @@ class QuestionPage extends Component{
 
     }
 
-    hasAnswered=()=>{
+    hasAnswered=()=> {
 
-        const { users, authUser, question } = this.props
+        const {users, authUser, question} = this.props
         let hasAnswer;
 
-        if(Object.keys(users[authUser].answers).includes(question.id)){
+        if (Object.keys(users[authUser].answers).includes(question.id)) {
             hasAnswer = true
             return hasAnswer
-        }
-        else{
+        } else {
             hasAnswer = false
             return hasAnswer
         }
-        return hasAnswer
-        }
+    }
 
     render() {
-        const { id, question, authUser } = this.props
+        const { id, question, authUser, users } = this.props
+        const { optionOne, optionTwo } = question
+
+        const optionOneVotes = optionOne.votes;
+        const optionTwoVotes = optionTwo.votes;
+
+        const totalVotes = optionOneVotes.length + optionTwoVotes.length;
+
+        function votes(option){
+            return Math.round(option.votes.length / totalVotes * 100)
+        }
+
+        const yourVote = users[authUser].answers[id];
+        console.log(yourVote)
+
+
         return(
             <div className="question-page">
+                <div>
                 Question Page
                 <Question id={id}/>
                 <button className={this.hasAnswered() === true ? 'disabled' : 'qpb'}
                     value="optionOne"
                     onClick={this.handleClick}
                     disabled={this.hasAnswered() === true}
-                >{question.optionOne.text}</button>
+                >{optionOne.text}</button>
                 <button className={this.hasAnswered() === true ? 'disabled' : 'qpb'}
                     value="optionTwo"
                     onClick={this.handleClick}
                     disabled={this.hasAnswered() === true}
-                >{question.optionTwo.text}</button>
-                {this.hasAnswered()=== true &&
+                >{optionTwo.text}</button>
+                </div>
                 <div>
-                this will be aprogres bar type thing
+                {this.hasAnswered()=== true &&
+                <div className="votesSection">
+                    <div className="progressBar">
+                        <span style={{ width: `${votes(optionOne)}%` }}>
+                            {votes(optionOne)}%
+                        </span>
+                        <p>{optionOneVotes.length} Votes of {totalVotes}</p>
+                     </div>
+                    <div className="progressBar">
+                        <span style={{ width: `${votes(optionTwo)}%` }}>
+                            {votes(optionTwo)}%
+                        </span>
+                        <p>{optionTwoVotes.length} Votes of {totalVotes}</p>
+                    </div>
                 </div>
                 }
+                </div>
             </div>
 
         )
