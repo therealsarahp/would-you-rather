069a69268bd1f84e2918015e690dd3b12baa8614
Sarah P <poultonse@gmail.com>
2021-08-27 13:06:54 -0500

    navbar looks less crappy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import {NavLink} from "react-router-dom";
+import {connect} from "react-redux";
 
 
-export default function Nav(){
-  return (
-      <nav className='nav'>
-        <ul>
-            <li>
-                <NavLink to='/' exact activeClassName='active'>
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/add' activeClassName='active'>
-                    New Question
-                </NavLink>
-            </li>
-            <li>
-            <NavLink to='/leaderboard' activeClassName='active'>
-                    LeaderBoard
-            </NavLink>
-            </li>
-            <li>
-                <NavLink to='/login' activeClassName='active'>
-                    Logout
-                </NavLink>
-            </li>
-        </ul>
-    </nav>
-  )
+class Nav extends Component{
+    render(){
+
+        return (
+
+            <nav className='nav'>
+                <ul>
+                    <div className='nav-left'>
+                        <li>
+                            <NavLink to='/' exact activeClassName='active'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/add' activeClassName='active'>
+                                New Question
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/leaderboard' activeClassName='active'>
+                                LeaderBoard
+                            </NavLink>
+                        </li>
+                    </div>
+                    <div className='nav-right'>
+                        <li>
+                                <span>Hello, {this.props.authUser}  </span>
+                                        <NavLink to='/login' activeClassName='active'>
+                                            <span>      Logout</span>
+                                        </NavLink>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+
+
+        )
+    }
 }
+
+function mapStateToProps({ authUser, users }){
+    return {
+        authUser,
+        users
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
